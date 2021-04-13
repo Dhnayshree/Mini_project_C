2f369ba902895bb2d390c87026ab9fd3b4ee9962
265351 (Dhanyashree) <80781718+Dhnayshree@users.noreply.github.com>
2021-04-13 19:27:20 +0530

    Ceaser Cipher Final(No more change.)

--- Ceaser_Cipher.c
+++ Ceaser_Cipher.c
@@ -1,7 +1,7 @@
-#include<stdio>
-//#include<stdlib.h>
-//#include<time.h>
-//#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
 
 struct Box
 {
@@ -13,7 +13,7 @@ struct Box
 int Generate_Secret_Key_1()
 {
     srand(time(NULL));
-    return (rend()%10);
+    return (rand()%10);
 }
 
 const char* Generate_Ceaser_Cipher(char str[], int sk1)
@@ -22,17 +22,11 @@ const char* Generate_Ceaser_Cipher(char str[], int sk1)
     {
         if(str[i]>= 'a' && str[i]<= 'z')
         {
-            str[i] = str[i] + sk1;
-
-            if(str[i]>'z')
-            str[i] = str[i] - 'z' + 'a' - 1;
+            str[i] = (char)((int)(str[i]+sk1-97)%26 +97);
         }
         else if(str[i]>= 'A' && str[i]<= 'Z')
         {
-            str[i] = str[i]+sk1;
-
-            if(str[i]>'Z')
-            str[i] = str[i] - 'Z' + 'A' - 1;
+            str[i] = (char)((int)(str[i]+sk1-65)%26 +65);
         }
     }
 
@@ -49,12 +43,13 @@ const char* calculate_Ceaser_Plain_Text(char str[], int sk1)
 
             if(str[i]<'a')
             str[i] = str[i] + 'z' - 'a' + 1;
+
         }
         else if(str[i]>= 'A' && str[i]<= 'Z')
         {
             str[i] = str[i] - sk1;
 
-            if(str[i]>'Z')
+            if(str[i]<'A')
             str[i] = str[i] + 'Z' - 'A' + 1;
         }
     }
